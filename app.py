fd39cb08530119c78f0a62361ee6f2820f4cba0f
RROUSHAN1602 <135798203+RROUSHAN1602@users.noreply.github.com>
2025-05-23 14:09:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ import plotly.graph_objects as go
 import pyotp
 from prophet import Prophet
 from scipy.signal import argrelextrema
-from SmartApi import SmartConnect
+from smartapi import SmartConnect
+
 
 st.set_page_config(page_title="Wave & Prophet Scanner", layout="wide")
 
